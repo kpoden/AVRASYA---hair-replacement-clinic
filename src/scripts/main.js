ee79fc48efdab5cea7107dee596973beb2113323
kpoden <67436313+kpoden@users.noreply.github.com>
2020-07-15 16:57:08 +0500

    mobil
    side menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-console.log(Hey);
+$(document).ready(function() {
+
+    // mobile menu script
+    $('.burger-menu').on('click', function(){
+        $('.mobile-menu').toggleClass("active");
+        $('.burger-menu').toggleClass("close");
+        $('.background-mobile').toggleClass("bgactive");
+    });
+    $(document).on('click', function(e) {
+        console.log($('.background-mobile').is(e.target)); 
+        if ($('.background-mobile').is(e.target)){
+          $('.mobile-menu').removeClass('active');
+          $('.burger-menu').removeClass("close");
+          $('.background-mobile').removeClass("bgactive");
+        }
+        e.stopPropagation();
+      });
+});
