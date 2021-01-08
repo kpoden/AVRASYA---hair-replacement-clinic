f53183e71450131e2c178750323514dffe721646
kpoden <67436313+kpoden@users.noreply.github.com>
2021-01-08 18:06:06 +0500

    mobile menu fixed
    doc  block fixed too

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -9,13 +9,16 @@ $(document).ready(function() {
     // mobile menu script
     $('.burger-menu').on('click', function(){
         $('.mobile-menu').toggleClass("active");
-        $('.burger-menu').toggleClass("close");
         $('.background-mobile').toggleClass("bgactive");
     });
+    $('.close').on('click', function(){
+        $('.mobile-menu').removeClass("active");
+        $('.background-mobile').removeClass("bgactive");
+    });
+
     $(document).on('click', function(e) {
         if ($('.background-mobile').is(e.target)){
           $('.mobile-menu').removeClass('active');
-          $('.burger-menu').removeClass("close");
           $('.background-mobile').removeClass("bgactive");
         }
         e.stopPropagation();
